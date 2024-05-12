fe14ef8351c40c4c364836cad086181ad0d52b0a
Gabriel Quintiliano Rosa pereira <gabrielq.pereira@gmail.com>
2024-05-12 16:55:17 -0300

    nexter - trying of color-theme changes [don't merge that]

--- nexter/sidebar.js
+++ nexter/sidebar.js
@@ -4,3 +4,15 @@ const sidebarToggleBtn = document.querySelector(".sidebar__toggle-btn");
 sidebarToggleBtn.addEventListener("click", () => {
   sidebar.classList.toggle("sidebar--opened");
 })
+
+const body = document.querySelector("body");
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+
+darkModeToggle.addEventListener("click", () => {
+  if (body.hasAttribute("dark-mode")) {
+    body.removeAttribute("dark-mode");
+  } else {
+    body.setAttribute("dark-mode", "");
+  }
+  
+})
